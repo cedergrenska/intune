6e93ac317e945e066e2892dfc65c36466c056a7d
tfg-matant <62142769+tfg-matant@users.noreply.github.com>
2020-09-10 10:19:52 +0200

    Create login.cmd

---
+++ scripts/login/login.cmd
@@ -0,0 +1,9 @@
+net use /persistent:no
+net use N: /delete
+\\tfg-srv-fs01\SYSVOL\tibble.gisab.org\scripts\IfMember NAS-Elever
+if not errorlevel 1 goto 2
+net use N: \\tfg-srv-fs01.skola.tfgab.se\tibnas01
+
+:2
+
+end
