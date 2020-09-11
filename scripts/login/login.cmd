8cb6e903e8753f529996b88503d074fb2437cb40
cedergrenska <70898645+cedergrenska@users.noreply.github.com>
2020-09-11 14:45:15 +0200

    Update login.cmd

--- scripts/login/login.cmd
+++ scripts/login/login.cmd
@@ -1,9 +1,9 @@
 net use /persistent:no
 net use N: /delete
-\\tfg-srv-fs01\SYSVOL\tibble.gisab.org\scripts\IfMember TibbleNas
-if not errorlevel 1 goto 2
-net use N: \\tfg-srv-fs01.skola.tfgab.se\tibnas01
 
+pushd \\TFG-SRV-FS01\TIBNAS01\ACCESS\GRANTED
+if errorlevel 1 goto 2
+net use N: \\TFG-SRV-FS01\TIBNAS01
 :2
-
-end
+popd
+pause
